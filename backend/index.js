3bcf71d91b43a01b8c1fef1a9ff4b9d80cf4b9e6
Haha <kmchung@cougarnet.uh.edu>
2024-11-18 16:13:44 -0600

    change report ticket to show no is deleted

--- backend/index.js
+++ backend/index.js
@@ -1691,7 +1691,6 @@ const getTicketReport = (startDate, endDate, exhibits, res) => {
       LEFT JOIN Ticket t ON e.ID = t.Exhibit_ID 
         AND t.Purchase_Date BETWEEN ? AND ?
         ${exhibitFilter}
-      WHERE e.is_deleted = 0
       GROUP BY e.ID, e.Name
       ORDER BY tickets DESC
     `,
